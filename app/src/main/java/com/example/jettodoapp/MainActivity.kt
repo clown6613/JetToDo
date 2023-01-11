b2de21bca27c7e58843ba307f9ac41d4705cd564
Kota Yoshikawa <clown.66137@gmail.com>
2023-01-11 09:32:49 +0900

    feat: create functions

--- app/src/main/java/com/example/jettodoapp/MainActivity.kt
+++ app/src/main/java/com/example/jettodoapp/MainActivity.kt
@@ -1,18 +1,24 @@
 package com.example.jettodoapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.jettodoapp.components.EditDialog
+import com.example.jettodoapp.components.TaskList
 import com.example.jettodoapp.ui.theme.JetTodoAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,10 +29,32 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Box{}
-
+                    MainContent()
                 }
             }
         }
+    }
+}
+
+@Composable
+fun MainContent(viewModel: MainViewModel = hiltViewModel()){
+    if(viewModel.isShowDialog){
+        EditDialog()
+    }
+
+    Scaffold(floatingActionButton = {
+        FloatingActionButton(onClick = { viewModel.isShowDialog= true }) {
+            Icon(imageVector = Icons.Default.Add, contentDescription = "新規作成")
+        }
+    }) {
+        val tasks by viewModel.tasks.collectAsState(initial = emptyList())
+        TaskList(
+            tasks = tasks,
+            onClickRow = {
+                viewModel.setEditingTask(it)
+                viewModel.isShowDialog = true
+            },
+            onClickDelete = { viewModel.deleteTask(it) }
+        )
     }
 }
---
+++ app/src/main/java/com/example/jettodoapp/components/TaskList.kt
@@ -0,0 +1,24 @@
+package com.example.jettodoapp.components
+
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.room.Delete
+import com.example.jettodoapp.Task
+
+@Composable
+fun TaskList(
+    tasks: List<Task>,
+    onClickRow:(Task) -> Unit,
+    onClickDelete: (Task) -> Unit
+){
+    LazyColumn{
+        items(tasks){ task ->
+            TaskRow(
+                task = task,
+                onClickRow = onClickRow,
+                onClickDelete = onClickDelete
+            )
+        }
+    }
+}
